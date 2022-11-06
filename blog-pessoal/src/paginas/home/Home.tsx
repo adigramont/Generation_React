caffb8c5f40c42f0695c0e44cc15427279877a1a
adigramont <hellen.eilert@yahoo.com.br>
2022-11-06 17:54:50 -0300

    react

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+  return (
+    <>
+    <h1 className='titulo'>Home</h1>
+   <img src="https://s2.glbimg.com/rnQu06wV6vDjZ6g1og13E8pyz-Q=/0x0:700x554/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2022/g/K/WAn5xnR3WAIzn7VTRDgQ/97702710-beyonce-knowles.jpg" alt="imagem tela inicial" className="img"/>
+    </>
+  );
+}
+
+export default Home;
