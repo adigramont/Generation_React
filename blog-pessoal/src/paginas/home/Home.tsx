fef34b115b484ccde9f4c0fe14ce7cb3fae0f0aa
adigramont <hellen.eilert@yahoo.com.br>
2022-11-07 17:35:21 -0300

    menu,rodapé,home

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,11 +1,35 @@
+import { Box, Button, Grid, TextField } from '@mui/material';
 import React from 'react';
 import './Home.css';
+import { Typography } from '@material-ui/core';
 
-function Home(){
+function Home() {
   return (
     <>
-    <h1 className='titulo'>Home</h1>
-   <img src="https://s2.glbimg.com/rnQu06wV6vDjZ6g1og13E8pyz-Q=/0x0:700x554/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2022/g/K/WAn5xnR3WAIzn7VTRDgQ/97702710-beyonce-knowles.jpg" alt="imagem tela inicial" className="img"/>
+      <>
+        <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: '#3F51B5' }}>
+          <Grid alignItems="center" item xs={6}>
+            <Box paddingX={20}>
+              <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: 'white', fontWeight: 'bold' }}>
+                Seja bem vindo(a)!
+              </Typography>
+              <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: 'white', fontWeight: 'bold' }}>
+                expresse aqui os seus pensamentos e opiniões!
+              </Typography>
+            </Box>
+            <Box display="flex" justifyContent="center">
+              <Box marginRight={1}></Box>
+              <Button variant="outlined" style={{ borderColor: 'white', backgroundColor: '#3F51B5', color: 'white', }}>
+                Ver Postagens
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <img src="https://i.imgur.com/wUf1NUF.png" alt="" width="500px" height="500px" />
+          </Grid>
+          <Grid xs={12} style={{ backgroundColor: 'white' }}></Grid>
+        </Grid>
+      </>
     </>
   );
 }
