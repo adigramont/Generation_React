12aded0149e06444c612b6afd0c0110200e5264a
adigramont <hellen.eilert@yahoo.com.br>
2022-11-11 01:43:34 -0300

    sistema de rotas

--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import {AppBar,Toolbar,Typography,} from '@material-ui/core';
+import { AppBar,Toolbar,Typography } from '@material-ui/core';
 import { Box, Grid } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -10,42 +10,43 @@ function Navbar() {
       <AppBar position="static">
         <Toolbar variant="dense">
           <Grid container justifyContent="space-between">
-            <Box style={{ cursor: 'pointer' }}>
+            <Box className='cursor'>
               <Typography variant="h5" color="inherit">
                 BlogPessoal
               </Typography>
             </Box>
 
             <Box display="flex" justifyContent="start">
-              <Box mx={1} style={{ cursor: 'pointer' }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   home
                 </Typography>
               </Box>
-              <Box mx={1} style={{ cursor: 'pointer' }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   postagens
                 </Typography>
               </Box>
-              <Box mx={1} style={{ cursor: 'pointer' }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   temas
                 </Typography>
               </Box>
-              <Box mx={1} style={{ cursor: 'pointer' }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   cadastrar tema
                 </Typography>
               </Box>
-              <Box mx={1} style={{ cursor: 'pointer' }}>
-                <Link to="/login" style={{ color: 'white' }}>
-                  <Typography variant="h6" color="inherit">
+              <Link to='/login' className='text-decorator-none'>
+              Box mx={1} className='cursor'>
+                <Typography variant="h6" color="inherit">
                     logout
                   </Typography>
-                </Link>
               </Box>
+              </Link>
+
             </Box>
-          </Grid>
+
         </Toolbar>
       </AppBar>
 
