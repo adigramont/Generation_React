e5f3629ec558bbc1af2640a92a42b232de9586d5
adigramont <hellen.eilert@yahoo.com.br>
2022-11-11 13:52:20 -0300

    login e cadastro

--- blog-pessoal/src/paginas/login/Login.tsx
+++ blog-pessoal/src/paginas/login/Login.tsx
@@ -12,7 +12,7 @@ function Login () {
           <Grid alignItems='center' xs={6}>
             <Box paddingX={20}>
               <form>
-                <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight: 'bold'}}>Entrar</Typography>
+                <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
                 <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidht />
                 <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidht />
                 <Box marginTop={2} textAlign='center'>
@@ -27,14 +27,13 @@ function Login () {
                 <Box marginRight={1}>
                   <Typography variant='subtitle1' gutterBotton align='center'>Não tem uma conta?</Typography>
                 </Box>
-                <Typography variant='subtitle2' gutterBotton align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
+                <Link to='/cadastrousuario'>
+                <Typography variant='subtitle2' gutterBotton align='center' className='textos'>Cadastre-se</Typography>
+                </Link>
               </Box>
             </Box>
           </Grid>
-          <Grid xs={6} style={{
-            backgroundImage: 'url()',
-            backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center' 
-          }}>
+          <Grid xs={6} className='imagem'>
 
 
           </Grid>
